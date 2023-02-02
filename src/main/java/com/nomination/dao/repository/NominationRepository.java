fe24bfecab7d9be8510ef1e47da64c852fd05707
garethpark <garethpark@yahoo.com>
2023-02-02 19:26:34 +0000

    added repository

---
+++ src/main/java/com/nomination/dao/repository/NominationRepository.java
@@ -0,0 +1,20 @@
+package com.nomination.dao.repository;
+
+import com.nomination.dao.entity.Nomination;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Set;
+
+@Repository
+public interface NominationRepository extends JpaRepository<Nomination, Long>, JpaSpecificationExecutor<Nomination> {
+
+    List<Nomination> findByIdIn(Set<Long> Id);
+    List<Nomination> findAllByStatus(String status);
+
+    //TODO do an explicit query
+    //@Query("SELECT")
+}
